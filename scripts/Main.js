a8451cc72c1b6968ebb23ae053c643a6b9c2d4a7
vincent.ly <vincent.ly@dps-fr.com>
2025-03-19 16:46:28 +0100

    Ajout d'un drag & drop pour l'ouverture de widget 3DPlay

--- scripts/Main.js
+++ scripts/Main.js
@@ -2,6 +2,8 @@ function executeWidgetCode(){
     require(["DS/i3DXCompassServices/i3DXCompassServices","DS/WAFData/WAFData"],function(i3DXCompassServices, WAFData){
         var myWidget = {
 
+            contentData : {},
+
             displayResult: function(data) {
                 console.log(data);
                 widget.getElement('#responseOutput').textContent = JSON.stringify(data, null, 2);
@@ -76,6 +78,21 @@ function executeWidgetCode(){
                 });
             },
 
+            handleClickOpenButton: function() {
+                i3DXCompassServices.getCompatibleApps({
+                    content: myWidget.contentData,
+                    onComplete: function(data) {
+                        console.log(data);
+                        for (let i=0; i<data.length; i++)
+                            if (data[i].name === "X3DPLAW_AP")
+                            {
+                                data[i].launchApp();
+                                break;
+                            }
+                    }
+                })
+            },
+
             onLoadWidget : function() {
                 widget.body.innerHTML = `<div class="RestRequestContainer">
                 <select name='method' id='method-select'>
@@ -122,6 +139,43 @@ function executeWidgetCode(){
                 });
 
                 widget.getElement('.response').appendChild(nodeCopyClipboard);
+
+                //Ajoute du drag & drop pour les tests d'ouverture de widget 3DX.
+                const dropElement = widget.createElement('div', {
+                    'class': 'droppableElement',
+                    html: "Drag file to this area to upload",
+                    styles: {
+                        border: "2px dashed",
+                        width : "auto",
+                        height : "auto",
+                        padding : "2px",
+                    },
+                });
+                widget.body.appendChild(dropElement);
+
+                const initialBorderStyle = dropElement.style.border;
+
+                DataDragAndDrop.droppable(dropElement, {
+                    enter: () => dropElement.style.border = '2px solid green',
+                    leave: () => dropElement.style.border = initialBorderStyle,
+                    over: () => console.log('Element is being dragged over the drop zone'),
+                    drop: (droppedData) => {
+                        const dataToSet = JSON.parse(droppedData);
+                        if (Object.keys(dataToSet).length > 0 && dataToSet.data.items.length > 0)
+                        {
+                            myWidget.contentData = dataToSet;
+                            const dataElement = widget.createElement('p', {
+                                'class': 'dataElement',
+                                html: myWidget.contentData.data.items[0].displayName,
+                                events: {
+                                    click: myWidget.handleClickOpenButton,
+                                },
+                            },);
+                            widget.body.removeChild(dropElement);
+                            widget.body.appendChild(dataElement);
+                        }
+                    }
+                })
             },
 
         }
